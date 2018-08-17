33f7553c039a873e2ccefb59c45ce258d480da66
Phoenix <theemaraldblock@gmail.com>
2018-08-17 23:06:18 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,21 @@
 const Discord = require("discord.js")
 const PhoenixAPI = new Discord.Client();
 const PhoenixAPIVER = ("Alpha Release V1")
+const PixBoot = new Discord.WebhookClient((process.env.BootID), (process.env.BootTO))
+const ErrorTypes = {
+  infoPrefix: ':information_source:',
+  errorPrefix: ':sos:',
+  successPrefix: ':white_check_mark:'
+}
+
 
 PhoenixAPI.on('ready', () => {
 PhoenixAPI.user.setActivity(`Phoenix API ${PhoenixAPIVER} `)
 console.log("PhoenixAPI has Connected Successfully to the Discord API")
+  PixBoot.send(`${ErrorTypes.successPrefix} System Has Started Up Successfully`);
+PixBoot.send(`${ErrorTypes.infoPrefix} System Is Running On Version Phoenix API & Logs ${PhoenixAPIVER}`);
 });
+
+
 
 PhoenixAPI.login(process.env.TOKEN)
