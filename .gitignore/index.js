1194a84eef995c151587c9d77e728d9f1786092e
Phoenix <theemaraldblock@gmail.com>
2018-08-18 11:24:23 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 const PhoenixAPI = new Discord.Client();
 const PhoenixAPIVER = ("Alpha Release V1")
 const PixBoot = new Discord.WebhookClient((process.env.BootID), (process.env.BootTO))
+const PixEmergency = new Discord.WebhookClient((process.env.EmergencyID), (process.env.EmergencyTO))
 const ErrorTypes = {
   infoPrefix: ':information_source:',
   errorPrefix: ':sos:',
@@ -16,6 +17,9 @@ console.log("PhoenixAPI has Connected Successfully to the Discord API")
 PixBoot.send(`${ErrorTypes.infoPrefix} System Is Running On Version Phoenix API & Logs ${PhoenixAPIVER}`);
 });
 
+PhoenixLogs.on('disconnected', Phoenix => {
+PixEmergency.send(`${ErrorTypes.errorPrefix} Phoenix API & Logs Has Been Disconnected From the Discord API`) 
+});
 
 
 PhoenixAPI.login(process.env.TOKEN)
